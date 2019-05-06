fb4aa857d5c98aff70d87051bb3af03250062953
admin <anthony@durrer.co.za>
2019-05-06 17:38:34 +0200

    New socket comm example

--- SocketComm/client.cpp
+++ SocketComm/client.cpp
@@ -1,44 +1,53 @@
-// Client side C/C++ program to demonstrate Socket programming
-#include <stdio.h>
 #include <sys/socket.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
 #include <string.h>
-#define PORT 8080
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-	struct sockaddr_in address;
-	int sock = 0, valread;
-	struct sockaddr_in serv_addr;
-	char *hello = "Hello from client";
-	char buffer[1024] = {0};
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("\n Socket creation error \n");
-		return -1;
-	}
+    int CreateSocket = 0,n = 0;
+    char dataReceived[1024];
+    struct sockaddr_in ipOfServer;
 
-	memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(dataReceived, '0' ,sizeof(dataReceived));
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+    if((CreateSocket = socket(AF_INET, SOCK_STREAM, 0))< 0)
+    {
+        printf("Socket not created \n");
+        return 1;
+    }
 
-	// Convert IPv4 and IPv6 addresses from text to binary form
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-	{
-		printf("\nInvalid address/ Address not supported \n");
-		return -1;
-	}
+    ipOfServer.sin_family = AF_INET;
+    ipOfServer.sin_port = htons(2017);
+    ipOfServer.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{
-		printf("\nConnection Failed \n");
-		return -1;
-	}
-	send(sock , hello , strlen(hello) , 0 );
-	printf("Hello message sent\n");
-	valread = read( sock , buffer, 1024);
-	printf("%s\n",buffer );
-	return 0;
+    if(connect(CreateSocket, (struct sockaddr *)&ipOfServer, sizeof(ipOfServer))<0)
+    {
+        printf("Connection failed due to port and ip problems\n");
+        return 1;
+    }
+
+    while((n = read(CreateSocket, dataReceived, sizeof(dataReceived)-1)) > 0)
+    {
+        dataReceived[n] = 0;
+        if(fputs(dataReceived, stdout) == EOF)
+        {
+            printf("\nStandard output error");
+        }
+
+        printf("\n");
+    }
+
+    if( n < 0)
+    {
+        printf("Standard input error \n");
+    }
+
+    return 0;
 }
--- SocketComm/server.cpp
+++ SocketComm/server.cpp
@@ -1,59 +1,41 @@
-// Server side C/C++ program to demonstrate Socket programming
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#define PORT 8080
-int main(int argc, char const *argv[])
+#include <stdio.h> // standard input and output library
+#include <stdlib.h> // this includes functions regarding memory allocation
+#include <string.h> // contains string functions
+#include <errno.h> //It defines macros for reporting and retrieving error conditions through error codes
+#include <time.h> //contains various functions for manipulating date and time
+#include <unistd.h> //contains various constants
+#include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
+#include <arpa/inet.h> // defines in_addr structure
+#include <sys/socket.h> // for socket creation
+#include <netinet/in.h> //contains constants and structures needed for internet domain addresses
+
+int main()
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char *hello = "Varible from Server";
+    time_t clock;
+	char dataSending[1025]; // Actually this is called packet in Network Communication, which contain data and send through.
+	int clintListn = 0, clintConnt = 0;
+	struct sockaddr_in ipOfServer;
+	clintListn = socket(AF_INET, SOCK_STREAM, 0); // creating socket
+	memset(&ipOfServer, '0', sizeof(ipOfServer));
+	memset(dataSending, '0', sizeof(dataSending));
+	ipOfServer.sin_family = AF_INET;
+	ipOfServer.sin_addr.s_addr = htonl(INADDR_ANY);
+	ipOfServer.sin_port = htons(2017); 		// this is the port number of running server
+	bind(clintListn, (struct sockaddr*)&ipOfServer , sizeof(ipOfServer));
+	listen(clintListn , 20);
 
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	while(1)
 	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+		printf("\n\nHi,Iam running server.Some Client hit me\n"); // whenever a request from client came. It will be processed here.
+		clintConnt = accept(clintListn, (struct sockaddr*)NULL, NULL);
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-												&opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+		clock = time(NULL);
+		snprintf(dataSending, sizeof(dataSending), "%.24s\r\n", ctime(&clock)); // Printing successful message
+		write(clintConnt, dataSending, strlen(dataSending));
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-								sizeof(address))<0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					(socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read( new_socket , buffer, 1024);
-	printf("%s\n",buffer );
-	send(new_socket , hello , strlen(hello) , 0 );
-	printf("Hello message sent\n");
-	return 0;
+        close(clintConnt);
+        sleep(1);
+     }
+
+     return 0;
 }
